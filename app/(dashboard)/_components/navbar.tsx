4fed53e7afd3e68757b8425bcd36116e7d04748d
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2023-10-21 13:19:41 +0700

    (feat) : layout

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import MobileSidebar from './mobile-sidebar';
+
+import NavbarRoutes from '@/components/navbar-routes';
+
+const Navbar = () => {
+  return (
+    <div className='p-4 border-b h-full flex items-center bg-white shadow-sm'>
+      <MobileSidebar />
+      <NavbarRoutes />
+    </div>
+  );
+};
+
+export default Navbar;
