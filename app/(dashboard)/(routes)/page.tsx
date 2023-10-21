9523fe50a398fd1b15db77ad66d4d5c6ae7b0424
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2023-10-21 11:35:35 +0700

    initial setup

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-xl text-sky-500">Home</h1>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
